68b7bc7748cab36a1ed718e8f7ba5a235b8ecd87
idei000sports <idei000sports@gmail.com>
2023-12-25 14:32:27 +0900

    tailwind

--- pages/index.js
+++ pages/index.js
@@ -106,9 +106,9 @@ export default function Home() {
       <Header />
 
       <div className="container px-5 py-12 mx-auto">
-        <div className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-2 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
-          <div className="relative flex-grow w-1/3">
-            <label htmlFor="mode" className="leading-7 text-sm text-gray-600">モード</label>
+        <div className="flex mx-auto w-full px-2 space-y-4 ">
+          <div className="flex-grow w-1/5">
+            <label htmlFor="mode" className="text-sm text-gray-600">モード</label>
             <Select
             name="mode"
             options={options}
@@ -116,13 +116,13 @@ export default function Home() {
             instanceId="mode"
             onChange={onChangeMode}
             defaultValue={options[0]}
-            className="h-24 w-full text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            className="w-full text-base outline-none text-gray-700 transition-colors duration-200 ease-in-out"
             />
           </div>
 
-          <div className="relative flex-grow w-2/3">
-            <label htmlFor="input" className="leading-7 text-sm text-gray-600">{info}</label>
-            <textarea type="text" id="textbox" name="input" onChange={onChangeWord} placeholder={sample} className="h-24 w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-transparent focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"></textarea>
+          <div className="flex-grow w-4/5">
+            <label htmlFor="input" className="text-sm text-gray-600">{info}</label>
+            <textarea type="text" id="textbox" name="input" onChange={onChangeWord} placeholder={sample} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-yellow-500 focus:bg-transparent focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 transition-colors duration-200 ease-in-out"></textarea>
           </div>
         </div>
       </div>
